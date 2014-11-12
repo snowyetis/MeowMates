c254d534606906be093fdb9f064d852b439028aa
josephcarlson <josephd.carlson@gmail.com>
2014-11-11 22:00:31 -0500

    Adding profiles

--- config/initializers/s3_direct_upload.rb
+++
@@ -1,7 +0,0 @@
-S3DirectUpload.config do |c|
-    c.access_key_id = "AKIAJQFFJPCGZSOIQWGQ"
-    c.secret_access_key = "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"
-    c.bucket = "purrrbucket"
-    c.region = "Oregon"
-    c.url = "https://#{c.bucket}.s3.amazonaws.com/"
-end
